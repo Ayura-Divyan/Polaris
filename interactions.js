b8afdd0f9fff5c6180a33216549f57cad294fe33
Nimhara Weerakkodi <nimhara.weerakkodi@gmail.com>
2026-02-14 22:14:59 +0530

    finished cta-button animation

--- interactions.js
+++ interactions.js
@@ -1,1 +1,9 @@
+//Animation for button press
+const button = document.getElementsByClassName('journey')[0];
 
+button.addEventListener('click', () => {
+  button.classList.add('pressed');
+  setTimeout(() => {
+    button.classList.remove('pressed');
+  }, 200); 
+});
