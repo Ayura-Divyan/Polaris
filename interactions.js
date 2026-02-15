f40a832db1c8bd00c84855e12bf68798d98a3f02
Nimhara Weerakkodi <nimhara.weerakkodi@gmail.com>
2026-02-15 12:44:25 +0530

    change of ID to class

--- interactions.js
+++ interactions.js
@@ -7,3 +7,22 @@ button.addEventListener('click', () => {
     button.classList.remove('pressed');
   }, 200); 
 });
+
+// Scroll-based class toggles (animation of elements when they enter the viewpoint)
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('show');
+      
+    } else {
+      entry.target.classList.remove('show');
+    }
+  });
+}, {
+  threshold: 0.1 
+});
+
+const scrollAnimation = document.getElementById('header');
+scrollAnimation.classList.add('hidden');
+observer.observe(scrollAnimation);
