07e840023ec493c58bc5fe1a2727fa5b0739896f
Nimhara Weerakkodi <nimhara.weerakkodi@gmail.com>
2026-02-16 12:19:38 +0530

    synced changes

--- nav.js
+++ nav.js
@@ -18,4 +18,34 @@ document.querySelector("#learn").addEventListener("click", function(e) {
 
   // Remove hash from URL
   history.pushState("", document.title, window.location.pathname);
+});document.querySelector("#started").addEventListener("click", function(e) {
+  e.preventDefault();
+
+  document.querySelector("#journey_button").scrollIntoView({
+    behavior: "smooth"
+  });
+
+  // Remove hash from URL
+  history.pushState("", document.title, window.location.pathname);
+});
+
+document.querySelector("#learn").addEventListener("click", function(e) {
+  e.preventDefault();
+
+  document.querySelector("#learn_more").scrollIntoView({
+    behavior: "smooth"
+  });
+
+  // Remove hash from URL
+  history.pushState("", document.title, window.location.pathname);
+});
+
+const navbar = document.querySelector(".main-navigation");
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 10) {
+      navbar.classList.add("scrolled");
+    } else {
+      navbar.classList.remove("scrolled");
+    }
 });
