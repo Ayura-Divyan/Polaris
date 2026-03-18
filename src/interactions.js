0e34ec61bb37b40d6ac5c3ab55922d93616b43a0
Nimhara Weerakkodi <nimhara.weerakkodi@gmail.com>
2026-03-18 15:19:17 +0530

    js refraction aborted. commit for initial state.

--- src/interactions.js
+++ src/interactions.js
@@ -1,64 +1,72 @@
 //Animation for button press
-const button = document.getElementsByClassName('journey')[0];
+const button = document.getElementsByClassName("journey")[0];
 
-button.addEventListener('click', () => {
-  button.classList.add('pressed');
+button.addEventListener("click", () => {
+  button.classList.add("pressed");
   setTimeout(() => {
-    button.classList.remove('pressed');
-  }, 200); 
+    button.classList.remove("pressed");
+  }, 200);
 });
 
-// Scroll-based class toggles 
+// Scroll-based class toggles
 // (animation of header when it enters the viewpoint)
 
-document.addEventListener('DOMContentLoaded', () => {
-  const observer = new IntersectionObserver((entries) => {
-    entries.forEach((entry) => {
-      if (entry.isIntersecting) {
-        entry.target.classList.add('show');
-      }
-    });
-  }, {
-    threshold: 0.1
-  });
+document.addEventListener("DOMContentLoaded", () => {
+  const observer = new IntersectionObserver(
+    (entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add("show");
+        }
+      });
+    },
+    {
+      threshold: 0.1,
+    },
+  );
 
-  const headerElements = document.querySelectorAll('.header');
+  const headerElements = document.querySelectorAll(".header");
   headerElements.forEach((el) => observer.observe(el));
 });
 
-// Scroll-based class toggles 
+// Scroll-based class toggles
 // (animation of featurelist when it enters the viewpoint)
 
-document.addEventListener('DOMContentLoaded', () => {
-  const observer = new IntersectionObserver((entries) => {
-    entries.forEach((entry) => {
-      if (entry.isIntersecting) {
-        entry.target.classList.add('show');
-      }
-    });
-  }, {
-    threshold: 0.1
-  });
+document.addEventListener("DOMContentLoaded", () => {
+  const observer = new IntersectionObserver(
+    (entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add("show");
+        }
+      });
+    },
+    {
+      threshold: 0.1,
+    },
+  );
 
-  const featureListElements = document.querySelectorAll('.featureList');
+  const featureListElements = document.querySelectorAll(".featureList");
   featureListElements.forEach((el) => observer.observe(el));
 });
 
-// Scroll-based class toggles 
+// Scroll-based class toggles
 // (animation of whyPolaris when it enters the viewpoint)
 
-document.addEventListener('DOMContentLoaded', () => {
-  const observer = new IntersectionObserver((entries) => {
-    entries.forEach((entry) => {
-      if (entry.isIntersecting) {
-        entry.target.classList.add('show');
-      }
-    });
-  }, {
-    threshold: 0.1
-  });
+document.addEventListener("DOMContentLoaded", () => {
+  const observer = new IntersectionObserver(
+    (entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add("show");
+        }
+      });
+    },
+    {
+      threshold: 0.1,
+    },
+  );
 
-  const whyPolarisElements = document.querySelectorAll('.whyPolaris');
+  const whyPolarisElements = document.querySelectorAll(".whyPolaris");
   whyPolarisElements.forEach((el) => observer.observe(el));
 });
-
--- src/nav.js
+++ src/nav.js
@@ -1,19 +1,19 @@
-document.querySelector("#started").addEventListener("click", function(e) {
+document.querySelector("#started").addEventListener("click", function (e) {
   e.preventDefault();
 
   document.querySelector("#journey_button").scrollIntoView({
-    behavior: "smooth"
+    behavior: "smooth",
   });
 
   // Remove hash from URL
   history.pushState("", document.title, window.location.pathname);
 });
 
-document.querySelector("#learn").addEventListener("click", function(e) {
+document.querySelector("#learn").addEventListener("click", function (e) {
   e.preventDefault();
 
   document.querySelector("#learn_more").scrollIntoView({
-    behavior: "smooth"
+    behavior: "smooth",
   });
 
   // Remove hash from URL
@@ -24,8 +24,8 @@ const navbar = document.querySelector(".main-navigation");
 
 window.addEventListener("scroll", () => {
   if (window.scrollY > 10) {
-      navbar.classList.add("scrolled");
-    } else {
-      navbar.classList.remove("scrolled");
-    }
+    navbar.classList.add("scrolled");
+  } else {
+    navbar.classList.remove("scrolled");
+  }
 });
